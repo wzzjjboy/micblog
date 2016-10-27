5e89b2ba9381822946f138609ee45d5217d5fdfa
Alan <wzzjjboy@126.com>
2016-10-27 11:01:39 +0800

    Flask 模板

--- micblog.py
+++ micblog.py
@@ -1,12 +1,26 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route('/')
 @app.route('/index')
 def hello_world():
-    return 'Hello World!'
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return flask.render_template('index.html',
+                                 title='alan',
+                                 user=user,
+                                 posts=posts)
 
 
 if __name__ == '__main__':
