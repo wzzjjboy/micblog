7b883ba130b20d0f12cbc9ec16a5956edfd44939
Alan <wzzjjboy@126.com>
2016-10-28 11:26:33 +0800

    Flask 表单

--- micblog.py
+++ micblog.py
@@ -1,6 +1,10 @@
 import flask
+from flask import flash
+from flask import redirect
+from forms import LoginForm
 
 app = flask.Flask(__name__)
+app.config.from_object('config')
 
 
 @app.route('/')
@@ -22,6 +26,18 @@ def hello_world():
                                  user=user,
                                  posts=posts)
 
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/index')
+    else:
+        flash('Login requested for Name: ' + str(form.name.data))
+        flash('passwd: ' + str(form.password.data))
+        flash('remember_me: ' + str(form.remember_me.data))
+
+    return flask.render_template('login.html', form=form, title="Sign In")
 
 if __name__ == '__main__':
     app.run(debug=True)
